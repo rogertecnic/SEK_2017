4cf62631b3a3dac407aacb4de98b5e862881f6ab
luluppy <luluppy@luluppy-Aspire-4349>
2017-08-31 08:23:17 -0300

    Controlador robo

--- teste/src_folder/Controlador_robo.cpp
+++ teste/src_folder/Controlador_robo.cpp
@@ -1,26 +1,16 @@
 #include "Controlador_robo.h"
 
-void nao_cai_porra () {
-	Controlador_motor rodaE(ev3dev::OUTPUT_A, 0.0408, 0.808, 0.3*0.826666667, true,  "debug_lego_E.m");
-	Controlador_motor rodaD(ev3dev::OUTPUT_B, 0.04058, 0.79, 0.3, true,  "debug_lego_D.m");
-	Sensor_cor  sensorE;
-	Sensor_cor sensorD;
-
-	rodaE.inicializa_thread();
-	rodaD.inicializa_thread();
-
-	rodaE.set_velo(0.3);
-	rodaD.set_velo(0.3);
-
-	usleep(1000*1000);
-	while (true){
-		if (sensorE.ler_cor()==nda || sensorD.ler_cor()==nda ) {
-
-		}
-	}
-
-
-	rodaE.finaliza_thread();
-	rodaD.finaliza_thread();
+void frente (){
 
 }
+
+void tras(){
+
+}
+
+
+void girar(Direcao dir){
+
+
+}
+
