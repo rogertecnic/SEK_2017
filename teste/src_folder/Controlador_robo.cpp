95d891f552435f4526049e8d1aad08022521b6e2
luanaguedes <luanagbmartins@gmail.com>
2017-09-06 17:54:39 -0300

    .

--- teste/src_folder/Controlador_robo.cpp
+++ teste/src_folder/Controlador_robo.cpp
@@ -1,24 +1,25 @@
 #include "Controlador_robo.h"
 
 Controlador_robo::Controlador_robo(){
-	rodaE = new Controlador_motor(ev3dev::OUTPUT_A, 0.0408, 0.808, 0.3*0.826666667, false,  "debug_lego_E.m");
-	rodaD = new Controlador_motor(ev3dev::OUTPUT_B, 0.04058, 0.79, 0.3, false,  "debug_lego_D.m");
+	rodaE = new Controlador_motor(ev3dev::OUTPUT_A, 0.0408, 0.808, false,  "debug_lego_E.m");
+	rodaD = new Controlador_motor(ev3dev::OUTPUT_B, 0.04058, 0.79, false,  "debug_lego_D.m");
 }
 
-void Controlador_robo::inicializar_thread() {
+void Controlador_robo::inicializar_thread_motor() {
 	rodaE->inicializa_thread();
 	rodaD->inicializa_thread();
 }
 
-void Controlador_robo::finalizar_thread(){
+void Controlador_robo::finalizar_thread_motor(){
 	rodaE->finaliza_thread();
 	rodaD->finaliza_thread();
 }
 
-void Controlador_robo::frente (){
-	rodaE->set_velo(0.3);
-	rodaD->set_velo(0.3);
-
+void Controlador_robo::frente (double velocidade, double aceleracao){
+	for (double i = 0; i <= velocidade; i=i+aceleracao){
+		rodaE->set_velo(i);
+		rodaD->set_velo(i);
+	}
 }
 
 void Controlador_robo::tras(){
@@ -36,16 +37,15 @@ void Controlador_robo::parar () {
 void Controlador_robo::girar(Direcao dir){
 	parar();
 	if(dir == Direcao::esquerda){
-		//rodaE.set_velo(-0.3);
-		//rodaD.set_velo(0.3);
-		//usleep(200*1000);
+		rodaE->set_velo(-0.3);
+		rodaD->set_velo(0.3);
+		usleep(200*1000);
 	}
 	else{
-		//rodaE.set_velo(0.3);
-		//rodaD.set_velo(-0.3);
-		//usleep(200*1000); //millis*1000
+		rodaE->set_velo(0.3);
+		rodaD->set_velo(-0.3);
+		usleep(200*1000); //millis*1000
 	}
 	parar();
 
 }
-
