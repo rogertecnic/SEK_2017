d4b5023cab0538dfd5cd77bdceb9b29c54ec7f6f
luanaguedes <luanagbmartins@gmail.com>
2017-09-06 14:26:15 -0300

    controlador robo

--- teste/src_folder/Controlador_robo.h
+++ teste/src_folder/Controlador_robo.h
@@ -3,16 +3,21 @@
 #include <iostream>
 #include "ev3dev.h"
 #include "Controlador_motor.h"
-#include "Sensor_cor.h"
 
 using namespace std;
 
 enum Direcao{esquerda, direita};
 
+Controlador_motor rodaE(ev3dev::OUTPUT_A, 0.0408, 0.808, 0.3*0.826666667, true,  "debug_lego_E.m");
+Controlador_motor rodaD(ev3dev::OUTPUT_B, 0.04058, 0.79, 0.3, true,  "debug_lego_D.m");
+
 class Controlador_robo {
 public:
+	void inicializar_thread();
+	void finalizar_thread();
 	void frente();
 	void tras();
+	void parar ();
 	void girar(Direcao);
 
 private:
