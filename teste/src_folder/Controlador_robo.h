d5a69338cb58d65f85a14b9d2d5fa57d39608bcc
luanaguedes <luanagbmartins@gmail.com>
2017-09-06 17:55:45 -0300

    .

--- teste/src_folder/Controlador_robo.h
+++ teste/src_folder/Controlador_robo.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "ev3dev.h"
 #include "Controlador_motor.h"
+#include <thread>
 
 using namespace std;
 
@@ -11,16 +12,15 @@ enum Direcao{esquerda, direita};
 class Controlador_robo {
 public:
 	Controlador_robo();
-	void inicializar_thread();
-	void finalizar_thread();
-	void frente();
+	void inicializar_thread_motor();
+	void finalizar_thread_motor();
+	void frente(double, double);
 	void tras();
 	void parar ();
 	void girar(Direcao);
 private:
 	Controlador_motor *rodaE;
 	Controlador_motor *rodaD;
-
 };
 
 
