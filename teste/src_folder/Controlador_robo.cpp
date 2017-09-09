5f2a2e4850d061a35b9ec4b48d48b76541aa44e2
luanaguedes <luanagbmartins@gmail.com>
2017-09-09 17:25:55 -0300

    .

--- teste/src_folder/Controlador_robo.cpp
+++ teste/src_folder/Controlador_robo.cpp
@@ -1,5 +1,6 @@
 #include "Controlador_robo.h"
 
+
 Controlador_robo::Controlador_robo(double fator_croda)
 :fator_croda(fator_croda)
 {
@@ -16,40 +17,20 @@ Controlador_robo::Controlador_robo(double fator_croda)
 
 
 void Controlador_robo::andar (int pwm_sp){
-	/*
-	velo_sp_me = velo_sp;
-	velo_sp_md = velo_sp;
-	 */
 	this->pwm_sp = pwm_sp;
 	estado = flag_aceleracao::linha_reta;
 }
 
 
 void Controlador_robo::parar () {
-	/*
-	velo_sp_me = 0.0;
-	velo_sp_md = 0.0;
-	 */
 	pwm_sp = abs(pwm_sp);
 	estado = flag_aceleracao::parar;
 
 }
 
+
 void Controlador_robo::girar(Direcao dir){
-	/*
-	parar();
-	if(dir == Direcao::esquerda){
-		rodaE->set_velo(-0.3);
-		rodaD->set_velo(0.3);
-		usleep(200*1000);
-	}
-	else{
-		rodaE->set_velo(0.3);
-		rodaD->set_velo(-0.3);
-		usleep(200*1000); //millis*1000
-	}
-	parar();
-	 */
+	//TODO
 
 }
 
@@ -61,23 +42,20 @@ bool Controlador_robo::inicializar_thread_aceleracao(){
 	return thread_rodando;
 }
 
+
 bool Controlador_robo::finalizar_thread_aceleracao(){
 	if(thread_rodando) thread_rodando = false;
 	usleep(1000*100);
 	return true;
 }
 
+
 void Controlador_robo::loop_controle_aceleracao(){
 	thread_rodando = true;
-	while (thread_rodando) {
-		/*  Controle de aceleração utilizando as variáveis:
-			velo_sp_me		velo_sp_md
-			Que serão atualizadas nos métodos: frente(), tras(), parar().
-		 */
 
+	while (thread_rodando) {
 		switch(estado){
 		case flag_aceleracao::linha_reta :
-			//while(true){
 			erro = rodaE->position()*fator_croda - rodaD->position();
 			pid = kp*erro + kd*(erro-erro_anterior);
 
@@ -99,90 +77,19 @@ void Controlador_robo::loop_controle_aceleracao(){
 				pwm_retardada += aceleracao*delay/1000;
 			if(pwm_retardada > pwm_sp+aceleracao*delay/1000)
 				pwm_retardada -= aceleracao*delay/1000;
+
 			cout<<pwm_retardada<<";"<<pwm_sp<<endl;
 
-			/*
-				if (velo_retardada_me <= velo_sp_me)
-					rodaE->set_velo(velo_retardada_me);
-				if (velo_retardada_md <= velo_sp_md)
-					rodaD->set_velo(velo_retardada_md);
-				usleep(1000*delay);
-				velo_retardada_me += aceleracao*(delay*0.001);
-				velo_retardada_md += aceleracao*(delay*0.001);
-				if (velo_retardada_me > velo_sp_me && velo_retardada_md > velo_sp_md)
-					break;
-			 */
-			//}
-			//estado = nda;
 			break;
 
-		case 5 : //  ->tras(velo_sp);
-			//			erro = rodaE->position()*fator_croda - rodaD->position();
-			//			pid = kp*erro + kd*(erro-erro_anterior);
-			//
-			//			pwm = pwm_retardada + pid;
-			//			if(pwm > 100) pwm = 100;
-			//			if(pwm < -100) pwm = -100;
-			//			rodaE->set_duty_cycle_sp(pwm);
-			//
-			//			pwm = pwm_retardada - pid;
-			//			if(pwm > 100) pwm = 100;
-			//			if(pwm < -100) pwm = -100;
-			//			rodaD->set_duty_cycle_sp(pwm);
-			//			usleep(1000*delay);
-			//
-			//			rodaE->run_direct();
-			//			rodaD->run_direct();
-			//
-			//			if(pwm_retardada > pwm_sp)
-			//				pwm_retardada -= aceleracao*delay/1000; // converte para miliseg
-			//			cout<<pwm_retardada<<";"<<pwm_sp<<endl;
-
-
-			//while(true){
-			//				if (velo_retardada_me >= velo_sp_me)
-			//					rodaE->set_velo(velo_retardada_me);
-			//				if (velo_retardada_md >= velo_sp_md)
-			//					rodaD->set_velo(velo_retardada_md);
-			//				usleep(1000*delay);
-			//				velo_retardada_me -= aceleracao*(delay*(0.001));
-			//				velo_retardada_md -= aceleracao*(delay*(0.001));
-			//				if (velo_retardada_me < velo_sp_me && velo_retardada_md < velo_sp_md)
-			//break;
-			//}
-
-			//flag_aceleracao = 0;
-			break;
-
-		case flag_aceleracao::parar: //  ->parar(0);
-			//while(true) {
-			//while(true){
-			//					if(velo_retardada_me > velo_sp_me)
-			//						rodaE->set_velo(velo_retardada_me);
-			//					if(velo_retardada_md > velo_sp_md)
-			//						rodaD->set_velo(velo_retardada_md);
-			//					usleep(1000*(delay*5));
-			//
-			//					if(velo_retardada_me < aceleracao*0.01   &&   velo_retardada_me > -aceleracao*0.01 ){
-			//						rodaE->set_velo(0);
-			//						velo_retardada_me = 0;
-			//						usleep(1000*(delay*5));
-			//					}
-			//					if(velo_retardada_md < aceleracao*0.01  &&   velo_retardada_md > -aceleracao*0.01 ){
-			//						rodaD->set_velo(0);
-			//						velo_retardada_md = 0.0;
-			//						usleep(1000*(delay*5));
-			//					}
-			//					velo_retardada_me -= aceleracao*(10*delay*(0.001));
-			//					velo_retardada_md -= aceleracao*(10*delay*(0.001));
-			//
-			//					if (velo_retardada_me == 0.0 && velo_retardada_md == 0.0)
-			//break;
-			//}
-
-			//}
+		case flag_aceleracao::parar :
 			rodaE->stop();
 			rodaD->stop();
+
+			pwm_sp = 0;
+			pwm_retardada = 0.0;
+			pwm = 0;
+
 			break;
 		}
 	}
--- teste/src_folder/Controlador_robo.h
+++ teste/src_folder/Controlador_robo.h
@@ -3,12 +3,12 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-#include "ev3dev.h"
 #include <unistd.h>
-
+#include "ev3dev.h"
 
 
 using namespace std;
+
 
 typedef chrono::high_resolution_clock Time;
 
@@ -19,7 +19,7 @@ enum flag_aceleracao{linha_reta, parar};
 
 class Controlador_robo {
 public:
-	Controlador_robo(double fator_croda);
+	Controlador_robo(double);
 
 	void andar(int);
 	void parar();
@@ -30,19 +30,14 @@ public:
 
 
 private:
-	/* Objetos da classe Controlador_motor*/
+	/*Roda esquerda e Roda direita do robô*/
 	ev3dev::large_motor *rodaE;
 	ev3dev::large_motor *rodaD;
+
 
 	/*Flag de controle do Switch da thread loop_controle_aceleracao*/
 	flag_aceleracao estado = flag_aceleracao::parar;
 
-	/*Variáveis de controle da velocidade de cada roda
-	double velo_retardada_me = 0.0;
-	double velo_retardada_md = 0.0;
-	double velo_sp_me = 0.0;
-	double velo_sp_md = 0.0;
-	 */
 
 	/*Thread: loop_controle_aceleracao*/
 	bool thread_rodando = false;
@@ -50,38 +45,30 @@ private:
 	void loop_controle_aceleracao();
 
 
-
-
-	/***********************************************************************************************/
-
-
 	/*Variaveis caracteristica do robo*/
 	double fator_croda;
+	double delay = 5.0;//Em miliseg
+	double aceleracao = 60.0;//Em pwm/seg
 
-	double delay = 5; // em miliseg
-	double aceleracao = 60; // em pwm/seg
 
 	/*Variáveis controlador PWM*/
-	double erro = 0;
-	double erro_anterior = 0;
-	int kp = 5; // 6
-	int kd = 1; // 1
+	double erro = 0.0;
+	double erro_anterior = 0.0;
+	int kp = 5;// 6
+	int kd = 1;
 	int pid = 0;
 
-	int pwm_sp = 0; // valor que eu quero
-	double pwm_retardada = 0; // valor que eu vou incrementar
-	int pwm = 0; // valor argumento da funcao "set_duty_cycle_sp
+	int pwm_sp = 0;//Valor desejado
+	double pwm_retardada = 0.0;//Valor que será incrementado
+	int pwm = 0;//Valor argumento da função "set_duty_cycle_sp"
 
 	/*Leitura de tempo p/ debug*/
 	chrono::system_clock::time_point t_inicial;
 	chrono::system_clock::time_point t_final;
-	chrono::duration<double> delta_t; // usar dt = t1-t2 e dt.count() para pegar o tempo em seg
+	chrono::duration<double> delta_t; //Usar dt = t1-t2 e dt.count() para pegar o tempo em seg
 	double tempo = 0;
 
 
-
 };
-
-
 
 #endif
